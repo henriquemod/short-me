6f4af5fbcdb44314119d243c5e41009060399b74
Henrique Souza <henrique.asouza@outlook.com>
2022-08-22 15:51:18 -0300

    chore(backend): config prettier and eslint

---
+++ packages/backend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:prettier/recommended',
+    'standard-with-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended'
+  ],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  rules: {
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/method-signature-style': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/indent': ['off'],
+    '@typescript-eslint/space-before-function-paren': ['off']
+  }
+}
