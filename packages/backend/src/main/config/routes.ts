7357c34e3e63e7acb9a5e4f0be307b22778d0079
Henrique Souza <henrique.asouza@pm.me>
2022-08-25 19:02:48 -0300

    chore(backend): handle diferent path for route depending on env

--- packages/backend/src/main/config/routes.ts
+++ packages/backend/src/main/config/routes.ts
@@ -1,12 +1,14 @@
-/* eslint-disable @typescript-eslint/no-unused-expressions */
-/* eslint-disable prettier/prettier */
 import { Express, Router } from 'express'
 import fb from 'fast-glob'
+import env from 'dotenv'
+env.config()
 
 export default (app: Express): void => {
+  const isDevelopment = process.env.ENV === 'development'
   const router = Router()
   app.use('/api', router)
-  fb.sync('**/src/main/routes/**routes.ts').map(async (file) =>
-    (await import(`../../../${file}`)).default(router)
-  )
+
+  fb.sync(
+    `**/${isDevelopment ? 'src' : 'build'}/main/routes/**routes.+(js|ts)`
+  ).map(async (file) => (await import(`../../../${file}`)).default(router))
 }
