a2a9a494e7feda58ec9f01afc3903d61a98bead9
Henrique Souza <henrique.asouza@outlook.com>
2022-09-12 14:18:19 -0300

    test(frontend): home page 100% coverage

--- packages/frontend/src/pages/home/home.test.tsx
+++ packages/frontend/src/pages/home/home.test.tsx
@@ -1,10 +1,62 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { act } from 'react-dom/test-utils'
 import { Home } from '.'
+
+const server = setupServer(
+    rest.post('http://localhost/undefined/api/url', (req, res, ctx) => {
+        return res(
+            ctx.json({
+                id: '02f68cc0-db56-4539-9ad9-78905a7fa470',
+                url: 'https://www.google.com.br',
+                key: 'wLzfZ'
+            })
+        )
+    })
+)
+
+beforeEach(() => {
+    server.listen()
+})
+
+afterEach(() => {
+    server.resetHandlers()
+})
+
+afterAll(() => {
+    server.close()
+})
 
 describe('Home unit tests', () => {
     test('renders home', () => {
         render(<Home />)
         const linkElement = screen.getByTestId('insert-url')
         expect(linkElement).toBeInTheDocument()
+    })
+    jest.mock('.')
+    test('should call copy to clipboard', async () => {
+        render(<Home />)
+
+        const insertInput = screen.getByRole('textbox')
+        const insertButton = screen.getByTestId('insert-button')
+
+        act(() => {
+            userEvent.type(insertInput, 'https://google.com.br')
+            fireEvent.click(insertButton)
+        })
+
+        const copyButton = await waitFor(async () =>
+            screen.findByTestId('copy-button')
+        )
+
+        act(() => {
+            fireEvent.click(copyButton)
+        })
+
+        expect(insertInput).toBeInTheDocument()
+        expect(insertButton).toBeInTheDocument()
+        expect(copyButton).toBeInTheDocument()
     })
 })
