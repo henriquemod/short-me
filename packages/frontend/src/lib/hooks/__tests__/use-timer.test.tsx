58de940220f59a09c26a806008ad681b17e1a501
Henrique Souza <henrique.asouza@pm.me>
2022-09-12 01:27:54 -0300

    test(frontend): use timer hook 100% coverage

---
+++ packages/frontend/src/lib/hooks/__tests__/use-timer.test.tsx
@@ -0,0 +1,30 @@
+import { act, renderHook } from '@testing-library/react'
+import { useTimer } from '../useTimer'
+
+jest.useFakeTimers()
+
+describe('Timer unit test', () => {
+    test('should initialize with correct time', () => {
+        const MOCK_TIME = 10
+        const { result } = renderHook(() => useTimer({ time: MOCK_TIME }))
+
+        expect(result.current.timeLimit).toBe(10)
+    })
+
+    test('should count time', async () => {
+        const spyInterval = jest.spyOn(global, 'setInterval')
+        const MOCK_TIME = 3
+        const { result } = renderHook(() => useTimer({ time: MOCK_TIME }))
+
+        expect(result.current.timeLimit).toBe(3)
+        expect(spyInterval).toBeCalledTimes(1)
+        await act(async () => {
+            for (let i = 0; i < 3; i++) {
+                jest.advanceTimersByTime(1000)
+                await Promise.resolve()
+            }
+        })
+
+        expect(spyInterval).toHaveBeenCalledTimes(2)
+    })
+})
