4565be9b16bd3f0e1ba19fa3a2e7a8de1aaa4334
Henrique Souza <henrique.asouza@outlook.com>
2022-09-12 12:57:30 -0300

    test(frontend): useNotification 100% coverage

---
+++ packages/frontend/src/lib/hooks/__tests__/use-notification.test.tsx
@@ -0,0 +1,59 @@
+import { Container } from '@mui/material'
+import {
+    act,
+    render,
+    renderHook,
+    screen,
+    waitFor
+} from '@testing-library/react'
+import { useNotification } from '../useNotification'
+
+jest.useFakeTimers()
+
+describe('useNotification hook unit tests', () => {
+    test('should show notification', async () => {
+        const { result } = renderHook(() => useNotification())
+        const spyNotification = jest.spyOn(result.current, 'notify')
+
+        act(() => {
+            result.current.notify('Test message', 'success')
+        })
+
+        render(<Container>{result.current.snack}</Container>)
+
+        const message = await waitFor(() => screen.findByText('Test message'))
+
+        expect(spyNotification).toBeCalledTimes(1)
+        expect(spyNotification).toHaveBeenCalledWith('Test message', 'success')
+        expect(message).toBeInTheDocument()
+    })
+
+    test('should hide notification', async () => {
+        const { result } = renderHook(() => useNotification())
+        const spyNotification = jest.spyOn(result.current, 'notify')
+
+        act(() => {
+            result.current.notify('Test message', 'success')
+        })
+
+        render(<Container>{result.current.snack}</Container>)
+
+        const message = await waitFor(() => screen.findByText('Test message'))
+        expect(spyNotification).toBeCalledTimes(1)
+        expect(spyNotification).toHaveBeenCalledWith('Test message', 'success')
+        expect(message).toBeInTheDocument()
+
+        act(() => {
+            jest.advanceTimersByTime(5000)
+        })
+
+        render(<Container>{result.current.snack}</Container>)
+
+        setTimeout(async () => {
+            const message = await waitFor(() =>
+                screen.findByText('Test message')
+            )
+            expect(message).not.toBeInTheDocument()
+        }, 1500)
+    })
+})
