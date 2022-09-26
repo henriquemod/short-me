6544932146dd15ed566e30af800beae53bbe9a29
Henrique Souza <henrique.asouza@outlook.com>
2022-09-26 01:29:44 -0300

    chore(frontend): add jest coverageThreshold and organize dependencies

---
+++ packages/frontend/jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+    verbose: true,
+    coverageThreshold: {
+        global: {
+            statements: 100,
+            branches: 95,
+            functions: 100,
+            lines: 100
+        }
+    }
+}
+
+export default config
