60dd5e2a9d74899d3dc8886c0e440920918f7157
Henrique Souza <henrique.asouza@outlook.com>
2022-10-01 19:54:41 -0300

    chore: more workflow changes

--- packages/loadingtest/src/index.ts
+++ packages/loadingtest/src/index.ts
@@ -5,6 +5,10 @@ import getUrl404Test from './url/get-404-status-test'
 export const options: Options = {
     vus: 500,
     duration: '10s',
+    thresholds: {
+        // http errors should be less than 5%
+        http_req_failed: ['rate<0.05']
+    },
     ext: {
         loadimpact: {
             projectID: 3603320,
