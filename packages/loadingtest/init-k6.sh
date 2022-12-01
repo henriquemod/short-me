178240281a0c7379ec511cd6c3046d66d318a727
unknown <guilhermemaffeidev@gmail.com>
2022-11-30 21:04:03 -0300

    update k6 script

--- packages/loadingtest/init-k6.sh
+++ packages/loadingtest/init-k6.sh
@@ -2,7 +2,7 @@
 
 curl https://github.com/grafana/k6/releases/download/v0.40.0/k6-v0.40.0-linux-amd64.tar.gz -L | tar xvz --strip-components 1
 
-#K6_CLOUD_TOKEN=<TOKEN> ./k6 run --out cloud ./dist/index.js
-./k6 run ./dist/index.js
+K6_CLOUD_TOKEN=<TOKEN> ./k6 run --out cloud ./dist/index.js
+# ./k6 run ./dist/index.js
 
 rm -rf ./k6
